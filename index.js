8580d3d4702dff6cf3334772c39b77f66d9bf4ef
DeeBest <simphiwedladla8@gmail.com>
2024-04-30 15:05:54 +0200

    added a function to update the textContent of the counter

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const counters = document.querySelectorAll('h3');
+
+counters.forEach((counter) => {
+  counter.textContent = 0;
+
+  incrementCounter();
+
+  function incrementCounter() {
+    let currentCounter = counter.textContent;
+    const dataCeil = counter.getAttribute('data-ceil');
+    const increment = dataCeil / 15;
+
+    currentCounter = Math.ceil(currentCounter + increment);
+
+    counter.textContent = currentCounter;
+  }
+});
