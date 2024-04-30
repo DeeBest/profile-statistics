1d127015770032518ddbab534daef0a3fc5627a4
DeeBest <simphiwedladla8@gmail.com>
2024-04-30 15:28:05 +0200

    added an if statement to stop the set timeout function when counter reaches the data ceil attribute

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
 const counters = document.querySelectorAll('h3');
 
 counters.forEach((counter) => {
-  counter.textContent = 0;
+  counter.textContent = '0';
 
   incrementCounter();
 
   function incrementCounter() {
-    let currentCounter = counter.textContent;
+    let currentCounter = +counter.textContent;
     const dataCeil = counter.getAttribute('data-ceil');
     const increment = dataCeil / 15;
 
     currentCounter = Math.ceil(currentCounter + increment);
 
-    counter.textContent = currentCounter;
+    if (currentCounter < dataCeil) {
+      counter.textContent = currentCounter;
+      setTimeout(incrementCounter, 60);
+    } else {
+      counter.textContent = dataCeil;
+    }
   }
 });
